0dea2055d9f99eb2f62312a1d880b1463638833d
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-11-19 21:42:03 +0100

    select card

--- client/src/Card/index.js
+++ client/src/Card/index.js
@@ -5,35 +5,57 @@ import Text from '../components/Text';
 import Card from './Card';
 
 const Table = styled.div`
-  align-items: center;
+  /* align-items: center;
   justify-content: center;
-  display: flex;
+  display: flex; */
 `;
 class index extends Component {
   state = {
     cards: [
-      { idx: 1, value: 0 },
-      { idx: 2, value: 0.5 },
-      { idx: 3, value: 1 },
-      { idx: 4, value: 2 },
-      { idx: 5, value: 3 },
-      { idx: 6, value: 5 },
-      { idx: 7, value: 8 },
-      { idx: 8, value: 13 },
-      { idx: 9, value: 20 },
-      { idx: 10, value: 40 },
-      { idx: 11, value: 100 },
+      { idx: 1, value: 0, selected: false },
+      { idx: 2, value: 0.5, selected: false },
+      { idx: 3, value: 1, selected: false },
+      { idx: 4, value: 2, selected: false },
+      { idx: 5, value: 3, selected: false },
+      { idx: 6, value: 5, selected: false },
+      { idx: 7, value: 8, selected: false },
+      { idx: 8, value: 13, selected: false },
+      { idx: 9, value: 20, selected: false },
+      { idx: 10, value: 40, selected: false },
+      { idx: 11, value: 100, selected: false },
     ],
+  };
+  select = e => {
+    const cards = this.state.cards.map(current => {
+      const newCard = { ...current };
+      if (current.idx === parseInt(e.target.dataset.idx)) {
+        newCard.selected = true;
+      } else {
+        newCard.selected = false;
+      }
+
+      return newCard;
+    });
+    this.setState({
+      cards,
+    });
   };
   render() {
     return (
-      <Table>
+      <div>
         {this.state.cards.map(card => (
-          <Card key={card.idx} idx={card.idx}>
+          <Card
+            data-idx={card.idx}
+            key={card.idx}
+            idx={card.idx}
+            pose={card.selected ? 'selected' : 'init'}
+            onTouchStart={this.select}
+            onMouseDown={this.select}
+          >
             <Text invert>{card.value}</Text>
           </Card>
         ))}
-      </Table>
+      </div>
     );
   }
 }
