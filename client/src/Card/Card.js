85dbb59bd85e873cde87d55c31060b4b116a74c4
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-11-19 21:41:57 +0100

    layout cards

--- client/src/Card/Card.js
+++ client/src/Card/Card.js
@@ -1,73 +1,77 @@
 import styled from 'styled-components';
 import posed from 'react-pose';
 
+const height = window.innerHeight;
+
 const transform = scale => props => {
   let deg = 0;
   let translateX = 0;
-  let translateY = 0;
+  let translateY = -height + 250;
   switch (props.idx) {
     case 1:
       deg = -90;
-      translateY = 200;
+      translateX -= 180;
+      translateY += 240;
       break;
     case 2:
       deg = -72;
-      translateY = 150;
+      translateX -= 170;
+      translateY += 180;
       break;
     case 3:
       deg = -54;
-      translateY = 110;
+      translateX -= 150;
+      translateY += 120;
       break;
     case 4:
       deg = -36;
-      translateY = 60;
+      translateX -= 110;
+      translateY += 70;
       break;
     case 5:
       deg = -18;
-      translateY = 30;
+      translateX -= 60;
+      translateY += 25;
       break;
 
     case 6:
       deg = 0;
+      translateX += 0;
+      translateY += 0;
       break;
 
     case 7:
       deg = 18;
-      translateY = 30;
+      translateX += 60;
+      translateY += 25;
       break;
     case 8:
       deg = 36;
-      translateY = 60;
+      translateX += 110;
+      translateY += 70;
       break;
     case 9:
       deg = 54;
-      translateY = 110;
+      translateX += 150;
+      translateY += 120;
       break;
     case 10:
       deg = 72;
-      translateY = 150;
+      translateX += 170;
+      translateY += 180;
       break;
     case 11:
       deg = 90;
-      translateY = 200;
+      translateX += 180;
+      translateY += 240;
       break;
 
     default:
       deg = 0;
-      translateY = 0;
+      translateX += 0;
+      translateY += 0;
       break;
   }
-  if (props.idx < 2) translateY += 20;
-  if (props.idx > 10) translateY += 20;
-
-  translateX = Math.floor(translateY / 2);
-  if (props.idx > 6) {
-    translateX *= -1;
-  }
-  if (props.idx < 3) translateX += 20;
-  if (props.idx < 2) translateX += 20;
-  if (props.idx > 9) translateX -= 20;
-  if (props.idx > 10) translateX -= 20;
 
   return `scale(${scale}) translateX(${translateX}px) translateY(${translateY}px) rotate(${deg}deg)`;
 };
@@ -83,18 +87,25 @@ const Card = styled(
     },
     hover: {
       boxShadow: '0px 5px 10px rgba(220,220,220,0.2)',
-      // transform: 'scale(1.2)',
       transform: transform(1.2),
+      // transform: 'scale(1.2)',
       'z-index': 1000,
     },
     press: {
       boxShadow: '0px 2px 5px rgba(220,220,220,0.1)',
-      // transform: 'scale(1.1)',
-      transform: transform(1.1),
+      transform: 'scale(1.1) translateX(0px) translateY(-20px) rotate(0deg)',
+      'z-index': 1000,
+    },
+    selected: {
+      boxShadow: '0px 2px 5px rgba(220,220,220,0.1)',
+      transform: 'scale(1.1) translateX(0px) translateY(-20px) rotate(0deg)',
       'z-index': 1000,
     },
   }),
 )`
+  position: fixed;
+  bottom: 0;
+  left: calc(50% - 50px);
   color: ${({ theme }) => (theme.mode === 'dark' ? theme.dark : theme.light)};
   background-color: ${({ theme }) =>
     theme.mode === 'dark' ? theme.light : theme.dark};
