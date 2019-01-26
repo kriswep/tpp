6a196f0eb475880400a8e9310e03595ad712313a
Christoph Benjamin Weber <kriswep@wetainment.com>
2019-01-26 19:45:33 +0100

    inform for playing state

--- client/src/Card/Master.js
+++ client/src/Card/Master.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ScreenCenterer from '../components/ScreenCenterer';
 import Button from '../components/Button';
 import Explain from '../components/Explain';
+import Wait from '../components/Wait';
 
 const Listen = ({ gameState, next }) => {
   if (!gameState) return null;
@@ -10,6 +11,7 @@ const Listen = ({ gameState, next }) => {
   return (
     <ScreenCenterer>
       {gameState.matches('intro') && <Explain noCenter={true} />}
+      {gameState.matches('play') && <Wait noCenter={true} />}
       {next && (
         <Button align="flex-end" size="big" onClick={next}>
           Next
