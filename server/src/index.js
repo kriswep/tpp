07dfc34a6efbb23f45ef2a3af1a7568f1b1c87b3
Christoph Benjamin Weber <kriswep@wetainment.com>
2019-02-02 13:10:21 +0100

    log some connection infos

--- server/src/index.js
+++ server/src/index.js
@@ -6,17 +6,28 @@ const server = new WebsocketServer({ port: 3210, clientTracking: false }, e =>
   console.log('Server listening'),
 );
 
-server.on('connection', socket => {
+server.on('connection', (socket, req) => {
   socket.on('message', msg => {
+    const ip =
+      (req &&
+        (req.headers['x-forwarded-for'] &&
+          req.headers['x-forwarded-for'].split(/\s*,\s*/)[0])) ||
+      (req.connection && req.connection.remoteAddress);
     let message;
     try {
       message = JSON.parse(msg);
     } catch (e) {}
     if (message && message.type === 'host' && message.channel) {
       if (channels.get(message.channel)) {
+        console.log(
+          `Host ${ip} tried to connect channel ${
+            message.channel
+          }. But it was used.`,
+        );
         return error(socket, 'channel used');
       }
       socket.channel = message.channel;
+      console.log(`Host ${ip}  in channel ${socket.channel} connected.`);
       return channels.set(message.channel, [
         { type: message.type, name: message.name, socket },
       ]);
@@ -24,9 +35,16 @@ server.on('connection', socket => {
     if (message && message.type === 'client' && message.channel) {
       const channel = channels.get(message.channel);
       if (!channel) {
+        console.log(
+          `Client ${ip}  tried to connect channel ${
+            socket.channel
+          }. But it didn't exist.`,
+        );
         return error(socket, 'channel unknown');
       }
       socket.channel = message.channel;
+      console.log(`Client ${ip}  in channel ${socket.channel} connected.`);
+
       return channels.set(message.channel, [
         ...channel,
         { type: message.type, name: message.name, socket },
@@ -46,7 +64,7 @@ server.on('connection', socket => {
         ) {
           return;
         }
-
+        console.log(`Signaling ${ip}  in channel ${socket.channel}: ${msg}`);
         other.socket.send(msg);
       });
     }
