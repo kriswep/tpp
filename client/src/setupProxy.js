6556e6d6960e662e68953d7a346b74f3e024d6af
Christoph Benjamin Weber <kriswep@wetainment.com>
2019-03-23 22:02:43 +0100

    netlify redirect to server

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,4 +2,11 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(proxy('/api/broker', { target: 'ws://localhost:3210', ws: true }));
+  // app.use(
+  //   proxy('/api/broker', {
+  //     target: 'https://api.andteam.app',
+  //     ws: true,
+  //     secure: false,
+  //   }),
+  // );
 };
