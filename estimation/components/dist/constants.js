531586c7ee2a00590b7c79fd66cd8ae9fb95fd9e
Christoph Benjamin Weber <kriswep@wetainment.com>
2019-04-11 21:02:45 +0200

    remove dist

--- estimation/components/dist/constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.socketUrl = void 0;
-const protocolPrefix = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
-const socketUrl = process.env.REACT_APP_SOCKET_URL ? process.env.REACT_APP_SOCKET_URL : `${protocolPrefix}${window.location.host}/api/broker`;
-exports.socketUrl = socketUrl;
