66b2cfe0f89de952b7e9dc858441337383b7baf0
Christoph Benjamin Weber <kriswep@wetainment.com>
2019-06-21 21:08:55 +0200

    remove experiments

--- estimation/components/src/Chat/ChatMessage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import Text from '../components/Text';
-
-const ChatMessage = props => {
-  return <Text>{props.message}</Text>;
-};
-
-export default ChatMessage;
--- estimation/components/src/constants.js
+++
@@ -1,5 +0,0 @@
-const protocolPrefix = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
-
-export const socketUrl = process.env.REACT_APP_SOCKET_URL
-  ? process.env.REACT_APP_SOCKET_URL
-  : `${protocolPrefix}${window.location.host}/api/broker`;
